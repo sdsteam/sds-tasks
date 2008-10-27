48adbd647887f24882f651098036347803842a68
James Avery <jamesavery@gmail.com>
2008-10-27 15:04:07 -0400

    Updated README

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,5 @@
 class Task < SDSActiveResource::Base
-  self.site = 'http://authority.data.database.windows.net/container/'
+  self.site = 'https://authority.data.database.windows.net/container/'
   
   #Username and password can be configured here or in the ssds.yml file
   #self.user = 'username'
